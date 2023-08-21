7f4df6558f623bedd44c36c4c26d83db69ebe576
Aiko <aikocute@icloud.com>
2023-08-21 20:35:02 +0700

    Update Cmd x25519
    - Update x25519
    
    log test :
    === RUN   Test_executeX25519
    Do you want to generate a key based on the node information?(Y/n)
    Private key: 0Z8a28li2HWRXP6rSeqUpsu2WNj22dHt-F2CJS-xZeI
    Public key: xZ1cjhk5KgY783u0nqLza1Cyy7llXPeDWL2JK0toOWE
    
    --- PASS: Test_executeX25519 (0.00s)
    PASS
    ok      github.com/AikoPanel/Aiko-Server/cmd    0.505s

---
+++ cmd/x25519.go
@@ -0,0 +1,66 @@
+package cmd
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+	"strings"
+
+	"github.com/AikoPanel/Aiko-Server/common/crypt"
+
+	"github.com/spf13/cobra"
+	"golang.org/x/crypto/curve25519"
+)
+
+var x25519Command = cobra.Command{
+	Use:   "x25519",
+	Short: "Generate key pair for x25519 key exchange",
+	Run: func(cmd *cobra.Command, args []string) {
+		executeX25519()
+	},
+}
+
+func init() {
+	command.AddCommand(&x25519Command)
+}
+
+func executeX25519() {
+	var output string
+	var err error
+	defer func() {
+		fmt.Println(output)
+	}()
+	var privateKey []byte
+	var publicKey []byte
+	var yes, key string
+	fmt.Println("Do you want to generate a key based on the node information?(Y/n)")
+	fmt.Scan(&yes)
+	if strings.ToLower(yes) == "y" {
+		var temp string
+		fmt.Println("Please enter the node ID:")
+		fmt.Scan(&temp)
+		key = temp
+		fmt.Println("Please enter the node type:")
+		fmt.Scan(&temp)
+		key += strings.ToLower(temp)
+		fmt.Println("Please enter the Token:")
+		fmt.Scan(&temp)
+		key += temp
+		privateKey = crypt.GenX25519Private([]byte(key))
+	} else {
+		privateKey = make([]byte, curve25519.ScalarSize)
+		if _, err = rand.Read(privateKey); err != nil {
+			output = Err("read rand error: ", err)
+			return
+		}
+	}
+	if publicKey, err = curve25519.X25519(privateKey, curve25519.Basepoint); err != nil {
+		output = Err("gen X25519 error: ", err)
+		return
+	}
+	p := base64.RawURLEncoding.EncodeToString(privateKey)
+	output = fmt.Sprint("Private key: ",
+		p,
+		"\nPublic key: ",
+		base64.RawURLEncoding.EncodeToString(publicKey))
+}
